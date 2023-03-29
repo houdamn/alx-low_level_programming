91c798a30a282fe0cb3db985a615b82f32012854
houdamn <houda.m1999@gmail.com>
2023-03-28 18:11:01 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	write(STDOUT_FILENO, str, len);
+	write(STDOUT_FILENO, "\n", 1);
+}
