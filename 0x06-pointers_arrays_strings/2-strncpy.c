580607ce98a33bb793776ce7e893e95a43282305
houdamn <houda.m1999@gmail.com>
2023-03-30 18:02:55 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strncpy - copies a string
+ * @dest: the buffer to copy the string to
+ * @src: the string to be copied
+ * @n: the maximum number of bytes to copy
+ *
+ * Return: a pointer to the destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return dest;
+}
+
