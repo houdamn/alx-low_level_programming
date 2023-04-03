7e2cae1b01bdde4d1a293801029f0f1e5c46147c
houdamn <houda.m1999@gmail.com>
2023-04-03 10:02:35 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+/**
+* _strstr - Entry point
+* @haystack: the string to search in
+* @needle: the substring to search for
+*
+* Return: Always 0 (Success)
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	char *h, *n;
+
+	while (*haystack)
+	{
+		h = haystack;
+		n = needle;
+
+		while (*haystack && *n && *haystack == *n)
+		{
+			haystack++;
+			n++;
+		}
+
+		if (!*n)
+			return (h);
+
+		haystack = h + 1;
+	}
+
+	return (NULL);
+}
+
+
