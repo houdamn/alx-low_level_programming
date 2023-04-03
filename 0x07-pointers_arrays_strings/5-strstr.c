e40380e6f608a80519f45f955e892c73965ab8c5
houdamn <houda.m1999@gmail.com>
2023-04-03 11:33:04 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,32 +1,33 @@
+#include <stddef.h>
+
 /**
-* _strstr - Entry point
-* @haystack: the string to search in
-* @needle: the substring to search for
-*
-* Return: Always 0 (Success)
-*/
+ * _strstr - Entry point
+ * @haystack: the string to search in
+ * @needle: the substring to search for
+ *
+ * Return: Always 0 (Success)
+ */
 char *_strstr(char *haystack, char *needle)
 {
-	char *h, *n;
+	char *haystack_ptr, *needle_ptr;
 
 	while (*haystack)
 	{
-		h = haystack;
-		n = needle;
+		haystack_ptr = haystack;
+		needle_ptr = needle;
 
-		while (*haystack && *n && *haystack == *n)
+		while (*haystack_ptr && *needle_ptr && *haystack_ptr == *needle_ptr)
 		{
-			haystack++;
-			n++;
+			haystack_ptr++;
+			needle_ptr++;
 		}
 
-		if (!*n)
-			return (h);
+		if (!*needle_ptr)
+			return (haystack);
 
-		haystack = h + 1;
+		haystack++;
 	}
 
 	return (NULL);
 }
-
 
